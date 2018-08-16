3beca7090e251f8230d48e539d3d25253e8bdb89
Loïc Prieto <loic.sephiroth@gmail.com>
2018-08-16 19:44:20 +0200

    Explain a little bit better what the audit factory is doing

--- src/main/java/org/sephire/training/annotations/audit/AuditFactory.java
+++ src/main/java/org/sephire/training/annotations/audit/AuditFactory.java
@@ -7,16 +7,24 @@ import net.sf.cglib.proxy.MethodInterceptor;
 public class AuditFactory {
 
   public static <T> T get(Class<T> auditedClass) {
+    // This guy will modify the class to give them audit super powers
     Enhancer enhancer = new Enhancer();
     enhancer.setSuperclass(auditedClass);
+
+    // The audited class will execute the code inside this callback instead
+    // of its own code
     enhancer.setCallback((MethodInterceptor) (obj, method, args, proxy) -> {
-      // We just enhance the methods that are marked with Audit annotation
+
+      // We will only modify methods that are marked with the @Audit annotation
       boolean isAuditMethod = Arrays.stream(method.getDeclaredAnnotations())
           .anyMatch(annotation -> {
             return annotation.annotationType().getName().equals(Audit.class.getName());
           });
+
+      // Check wether we should return something from the function
       boolean methodReturnsSomething = !method.getReturnType().equals(Void.TYPE);
 
+      // The class will execute this
       if (isAuditMethod) {
         String argsString = Arrays.stream(args)
             .reduce((arg1, arg2) -> arg1.toString() + ", " + arg2.toString())
@@ -26,17 +34,20 @@ public class AuditFactory {
         System.out.println("Method " + method.getName() + " was called with args " + argsString);
       }
 
+      // And also its original code
       Object result = proxy.invokeSuper(obj, args);
 
+      // Also log the result of the method if it has any
       if (isAuditMethod && methodReturnsSomething) {
         System.out.println("The method returned value " + result.toString());
       }
 
+      // Give back control to the caller
       return result;
     });
 
+    // Create and return an instance of the modified class
     T auditedService = (T) enhancer.create();
-
     return auditedService;
   }
 }
